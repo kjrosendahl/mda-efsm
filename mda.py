44d6719307791808e30b4aea3c63cdd7f6ff221f
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2023-04-23 15:01:38 -0500

    create mda states and op

--- mda.py
+++ mda.py
@@ -1,91 +1,156 @@
 from collections import defaultdict
+from op import op 
 
 class mda_efsm: 
     
     def __init__(self): 
-        self.LS = [0,0,0,0]
+
+        # initialize list of states 
+        self.LS = [start(self), no_cups(self), idle(self), ready(self)]
+        # initialize pointer to start state 
         self.S = self.LS[0]
-        return
 
+        # initialize cups and additive list 
+        self.cups = 0 
+        self.A = None 
+
+        print('MDA-EFSM created with', self.cups, 'cups')
+        
     def change_state(self, ID): 
+        old = self.S 
         self.S = self.LS[ID] 
+        new = self.S 
+        print('Transition from state ', old, ' to state ', new)
     
-class states: 
-
-    def __init__(self, m): 
-        self.d = mda_data()
-        self.m = m 
-        # add OP pointer 
-        self.p = 1 
-
     def create(self): 
-        return 
+        self.S.create() 
 
     def insert_cups(self, n: int): 
-        return 
+        self.S.insert_cups(n) 
     
     def coin(self, f: int): 
-        return 
+        self.S.coin(f)  
     
     def card(self): 
-        return 
+        self.S.card()  
     
     def cancel(self): 
-        return 
+        self.S.cancel()  
     
     def set_price(self): 
-        return 
+        self.S.set_price()  
     
     def dispose_drink(self, d: int): 
-        return 
+        self.S.dispose_drink(d) 
 
     def additive(self, a: int): 
-        return 
-
+        self.S.additive(a) 
     
+class states(): 
+
+    def __init__(self, m):
+
+        # set MDA-EFSM pointer for state 
+        self.m = m 
+        # add OP pointer 
+        self.p = op() 
+
+    # abstract methods 
+    def create(self): 
+        pass 
+
+    def insert_cups(self, n: int): 
+        pass 
+    
+    def coin(self, f: int): 
+        pass 
+    
+    def card(self): 
+        pass 
+    
+    def cancel(self): 
+        pass 
+    
+    def set_price(self): 
+        pass 
+    
+    def dispose_drink(self, d: int): 
+        pass 
+
+    def additive(self, a: int): 
+        pass 
+
 class start(states): 
 
     def create(self): 
-        self.p.create()  
+        self.p.StorePrice() 
+        # transition to no_cups state 
+        self.m.change_state(1)
 
 class no_cups(states): 
 
     def insert_cups(self, n: int): 
-        self.p.insert_cups(n)  
-    
-    def coin(self, f: int): 
-        self.p.coin(f)
+        print('Inserting', n, 'cups')
+        # increase cups 
+        self.m.cups += n
+        print('Total cups:', self.m.cups)
+        # transition to idle state  
+        self.m.change_state(2)
+        
+    def coin(self): 
+        self.p.ReturnCoins() 
 
 class idle(states): 
 
     def insert_cups(self, n: int): 
-        self.p.insert_cups(n) 
+        print('inserting ', n, 'cups')
+        # increase cups 
+        self.m.cups += n 
+        print('Total cups:', self.m.cups)
     
     def coin(self, f: int): 
-        self.p.coin(f)  
+        self.p.IncreaseCF() 
+        # if enough funds: 
+        if f == 1: 
+            # transition to ready state 
+            self.m.change_state(3)
+            # reset additives list 
+            self.m.A = defaultdict(int)
     
     def card(self): 
-        self.p.card()  
+        self.p.ZeroCF()
+        # transition to ready state 
+        self.m.change_state(3) 
+        # reset additives list 
+        self.m.A = defaultdict(int)
 
     def set_price(self): 
-        self.p.set_price()  
+        self.p.StorePrice() 
 
 class ready(states): 
 
     def coin(self, f: int): 
-        self.p.coin(f)  
+        self.p.ReturnCoins() 
     
     def cancel(self): 
-        self.p.cancel() 
+        self.p.ReturnCoins() 
+        self.p.ZeroCF() 
+        # transition back to idle state 
+        self.m.change_state(2)
 
     def dispose_drink(self, d: int): 
-        self.p.dispose_drink(d) 
+        self.p.DisposeDrink(d)
+        self.p.DisposeAdditive(self.m.A)
+        self.m.cups -= 1 
+        # if out of cups 
+        if self.m.cups == 0: 
+            # transition to no_cups state 
+            self.m.change_state(1)
+        else: 
+            self.p.ZeroCF() 
+            # transition to idle state 
+            self.m.change_state(2)
 
     def additive(self, a: int): 
-        self.p.dispose_drink(a)  
-    
-class mda_data: 
-    
-    def __init__(self): 
-        self.cups == 0 
-        self.A = defaultdict(int) 
+        # swap additive in additive list (T->F or F->T)
+        self.m.A[a] =  not self.m.A[a]
---
+++ op.py
@@ -0,0 +1,31 @@
+class op: 
+
+    def __init__(self): 
+        self.d = None 
+        self.p1 = None 
+        self.p2 = None 
+        self.p3 = None 
+        self.p4 = None 
+        self.p5 = None 
+        self.p6 = None 
+
+    def Initialize(self, af): 
+        pass 
+
+    def StorePrice(self): 
+        pass 
+
+    def ZeroCF(self): 
+        pass 
+
+    def IncreaseCF(self): 
+        pass 
+
+    def ReturnCoins(self): 
+        pass 
+
+    def DisposeDrink(self, d: int): 
+        pass 
+
+    def DisposeAdditive(self, A):
+        pass  
